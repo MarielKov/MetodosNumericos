720fc5d0458be4a2fb667076a73be522f788cd68
MarielKov <110503073+MarielKov@users.noreply.github.com>
2023-04-27 00:42:49 -0300

    Jacobi 2

---
+++ Jacobi.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+
+matrizA = pd.DataFrame([2,-1,1],[3,3,9],[3,3,5])
+matrizB = pd.DataFrame([-1,0,4])
+
+print(matrizA)
+
+print(matrizB)
+
+
+#def ecuacion1():
+    #return 2x -y + z
+
+#def ecuacion2():
+    #return 3x +3y + 9z
+
+#def ecuacion3():
+    #return 3x +3y + 5z
+
+
